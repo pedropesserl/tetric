4df85f507c7c37a4412d08286678e8055a61ec34
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2023-07-14 01:08:12 -0300

    update matrix.h

--- src/matrix.h
+++ src/matrix.h
@@ -13,8 +13,8 @@ int leftmost_column(int m[SZ][SZ]);
 
 int rightmost_column(int m[SZ][SZ]);
 
-void rotate_left(int (*m)[SZ][SZ]);
+void rotate_left(int m[SZ][SZ]);
 
-void rotate_right(int (*m)[SZ][SZ]);
+void rotate_right(int m[SZ][SZ]);
 
 #endif // MATRIX_H_
